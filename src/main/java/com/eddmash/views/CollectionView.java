789f905e610e14d7ddc70d0e85ada322e23f57d1
eddmash <edd.cowan@gmail.com>
2018-02-09 21:43:27 +0300

    remvove unused deps

--- src/main/java/com/eddmash/views/CollectionView.java
+++ src/main/java/com/eddmash/views/CollectionView.java
@@ -12,10 +12,8 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 
 import java.util.List;
 
