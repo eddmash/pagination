12b1555913a3ffda0f9fbd6a29b70825c891a210
eddmash <edd.cowan@gmail.com>
2018-02-10 12:36:22 +0300

    add docs

---
+++ src/main/java/com/eddmash/pagination/ListPaginator.java
@@ -0,0 +1,43 @@
+package com.eddmash.pagination;
+/*
+* This file is part of the com.eddmash.pagination package.
+* 
+* (c) Eddilbert Macharia (http://eddmash.com)<edd.cowan@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * An implimentation of {@link PaginatorInterface}
+ */
+public class ListPaginator extends Paginator {
+
+    private List<Map> _records = new ArrayList<>();
+
+
+    public ListPaginator(DataListener dataListener) {
+        super(dataListener);
+    }
+
+    public void setData(List<Map> results) {
+        Log.e(logTag, "SET DATA" + results.size());
+        _records = results;
+        _totalRecords = _records.size();
+        _currentRecordsCounter = pageSize;
+
+        new LoadDataTask(this).execute(0, pageSize);
+    }
+
+
+    protected List<Map> getNextPageRecords(int startPoint, int endPoint) {
+        return _records.subList(startPoint, endPoint);
+    }
+
+}
---
+++ src/main/java/com/eddmash/pagination/PaginatorInterface.java
@@ -0,0 +1,54 @@
+package com.eddmash.pagination;
+/*
+* This file is part of the com.eddmash.pagination package.
+* 
+* (c) Eddilbert Macharia (http://eddmash.com)<edd.cowan@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class that implement this interface help you manage paginated data can data that’s split
+ * across several pages.
+ * <p>
+ * {@link ListPaginator } this is used to resolve data that is not alot, i.e. data that can be
+ * held inthe memory with little to no cost on the app, this is usually data held in a List.
+ * <p>
+ * With large data that is usally stored in the database the {@link SqlPaginator} comes in handy,
+ * this paginator does not hold any data within it.
+ * <p>
+ * It only fetches the data and provides this data to you, its upto you to know how to store or
+ * display this data in an efficient manner.
+ */
+public interface PaginatorInterface {
+    /**
+     * The number of records to display per page.
+     *
+     * @param pageSize
+     */
+    void setPageSize(int pageSize);
+
+    /**
+     * This does the actual loading of data. The loading should be done asynchronously.
+     */
+    void fetchNextPageData();
+
+
+    /**
+     * The total number of pages.
+     *
+     * @return
+     */
+    int getPageCount();
+
+    /**
+     * The total number of records.
+     *
+     * @return
+     */
+    int getTotalRecords();
+}
